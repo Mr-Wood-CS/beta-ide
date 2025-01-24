8a7dc27c0db13c95e30bd161fdefc487eb6e165f
Mr-Wood-CS <77886276+Mr-Wood-CS@users.noreply.github.com>
2025-01-24 13:51:54 +0000

    update files

--- js/config.js
+++ js/config.js
@@ -10,6 +10,39 @@ const workspace = Blockly.inject('blocklyDiv', {
       <block type="math_arithmetic"></block>
       <block type="text"></block>
     </xml>
-  `,
-  horizontalLayout: true
+  `
 });
+
+const toolbox = {
+  "kind": "flyoutToolbox",
+  "contents": [
+    {
+      "kind": "block",
+      "type": "controls_if"
+    },
+    {
+      "kind": "block",
+      "type": "controls_repeat_ext"
+    },
+    {
+      "kind": "block",
+      "type": "logic_compare"
+    },
+    {
+      "kind": "block",
+      "type": "math_number"
+    },
+    {
+      "kind": "block",
+      "type": "math_arithmetic"
+    },
+    {
+      "kind": "block",
+      "type": "text"
+    },
+    {
+      "kind": "block",
+      "type": "text_print"
+    },
+  ]
+}
